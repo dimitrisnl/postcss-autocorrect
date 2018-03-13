4903e3182ccad33b47399eb8ba866f4f5af7f572
Dimitrios Lytras <dnlytras@gmail.com>
2018-03-13 20:25:46 +0200

    update to 1.3.2

--- index.js
+++ index.js
@@ -1,70 +1,50 @@
 'use strict';
-var postcss = require('postcss');
-var data = require('./lib/data');
+const postcss = require('postcss');
+const data = require('./lib/data');
 
-module.exports = postcss.plugin('postcss-autocorrect', function (opts) {
+module.exports = postcss.plugin('postcss-autocorrect', opts => {
+  opts = opts || {};
+  const shouldInitiateDefaults = !(opts.useDefaultList === false);
+  const providedList = opts.providedList || [];
 
-    opts = opts || {};
-    let mistakes = [];
-    let corrections = [];
+  const mistakes = shouldInitiateDefaults ? data.mistakes : [];
+  const corrections = shouldInitiateDefaults ? data.corrections : [];
 
-    let providedList = opts.providedList || [];
+  if (!!providedList && providedList.length > 0) {
+    providedList.forEach(obj => {
+      const key = Object.keys(obj)[0];
 
-    // If useDefaultList is true or undefined (by default true)
-    let shouldInitiateDefaults = !(opts.useDefaultList === false);
+      obj[key].forEach(value => {
+        mistakes.push(value);
+        corrections.push(key);
+      });
+    });
+  }
 
-    // Get the default values
-    if (shouldInitiateDefaults) {
-        mistakes = data.mistakes;
-        corrections = data.corrections;
+  const checkParameters = (property, line) => {
+    const isMistake = mistakes.indexOf(property);
+    if (isMistake > -1) {
+      let fix = corrections[isMistake];
+      console.warn(`Error in line ${line}.`);
+      console.warn(`Got ${property} instead of ${fix}`);
+      return fix;
     }
 
-    // if user has provided input,include them
-    if (!!providedList && providedList.length > 0) {
-        providedList.forEach(function (obj) {
+    return property;
+  };
 
-            let key = Object.keys(obj)[0];
+  return css => {
+    css.walkDecls(decl => {
+      // check properties
+      decl.prop = checkParameters(decl.prop, decl.source.start.line);
 
-            obj[key].forEach(function (value) {
-                mistakes.push(value);
-                corrections.push(key);
+      // check values
+      const values = decl.value.split(' ');
+      const line = decl.source.start.line;
 
-            });
-        });
-    }
+      const corrected = values.map(v => checkParameters(v, line));
 
-    function checkParameters(check, line) {
-        let isMistake = mistakes.indexOf(check);
-
-        if (isMistake !== -1) {
-            let fix = corrections[isMistake];
-            console.warn('Error in line ' + line + '.');
-            console.warn('Got "' + check + '" instead of "' + fix + '".');
-            return fix;
-        }
-
-        return check;
-    }
-
-    return function (css) {
-
-        css.walkDecls(function (decl) {
-
-            // check properties
-            decl.prop = checkParameters(decl.prop, decl.source.start.line);
-
-            // check values
-            const values = decl.value.split(' ');
-            const line = decl.source.start.line;
-
-            const corrected = values.map(v => {
-                return checkParameters(v, line);
-            });
-
-            decl.value = corrected.join(' ');
-
-        });
-
-    };
-
+      decl.value = corrected.join(' ');
+    });
+  };
 });
--- index.test.js
+++ index.test.js
@@ -1,40 +1,42 @@
 'use strict';
 
-var postcss = require('postcss');
-var plugin = require('./');
+const postcss = require('postcss');
+const plugin = require('./');
 
-function run(input, output, opts) {
-    return postcss([plugin(opts)]).process(input).then(result => {
-        expect(result.css).toEqual(output);
-        expect(result.warnings().length).toBe(0);
+const run = (input, output, opts) => {
+  return postcss([plugin(opts)])
+    .process(input)
+    .then(result => {
+      expect(result.css).toEqual(output);
+      expect(result.warnings().length).toBe(0);
     });
-}
+};
 
 it('prop mistake', () => {
-    return run('a{heigth:10px}', 'a{height:10px}', {});
+  return run('a{heigth:10px}', 'a{height:10px}', {});
 });
 
 it('prop & value mistake', () => {
-    return run('a{colour:blakc}', 'a{color:black}', {});
+  return run('a{colour:blakc}', 'a{color:black}', {});
 });
 
 it('with import', () => {
-    return run('a{dislay:flx;}', 'a{display:flex;}', {
-        useDefaultList: true,
-        providedList: [
-            {
-                flex: ['flx']
-            }
-        ]
-    });
+  return run('a{dislay:flx;}', 'a{display:flex;}', {
+    useDefaultList: true,
+    providedList: [
+      {
+        flex: ['flx']
+      }
+    ]
+  });
 });
 
 it('should not correct', () => {
-    return run('a{colour:blakc;}', 'a{colour:blakc;}', {
-        useDefaultList: false
-    });
+  return run('a{colour:blakc;}', 'a{colour:blakc;}', {
+    useDefaultList: false
+  });
 });
 
 it('many values', () => {
-    return run('a{border:10px solid blakc}', 'a{border:10px solid black}', {});
+  return run('a{border:10px solid blakc}', 'a{border:10px solid black}', {});
 });
